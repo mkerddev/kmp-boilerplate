f9e6be9a0cf525af38651e97a63b24a260c57dc0
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-12-25 16:41:54 +0100

    Update iOS target (Compose Multiplatform UI).

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,6 +16,17 @@ kotlin {
         }
     }
     
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
     sourceSets {
         
         androidMain.dependencies {
@@ -29,7 +40,6 @@ kotlin {
             implementation(compose.ui)
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
-            implementation(projects.shared)
         }
     }
 }
